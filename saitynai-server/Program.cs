1d989a8dcde6ccca2bc15691e011b8604b6e454f
Dzytizz <gytisdoksas@gmail.com>
2022-12-03 19:53:05 +0200

    CORS fix

--- saitynai-server/Program.cs
+++ saitynai-server/Program.cs
@@ -29,8 +29,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            //policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("http://127.0.0.1:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
         });
 });
 
